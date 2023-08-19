1c0ee62a9fabad8222c97dd696a143092fe6398f
tanu90141 <tanu90141@gmail.com>
2023-08-19 14:16:40 +0530

    react first class

---
+++ JAVASCRIPT/19aug.js
@@ -0,0 +1,27 @@
+// var pi=3.14;
+// function t(pi){
+//     console.log(pi);
+// }
+// t(pi);
+// console.log(pi);
+// var x=10;
+//  function t(){
+//     const x=10;
+//     if(true){
+//          const y=20;
+//         //  console.log(y);
+//    }
+//     console.log(x);
+//    console.log(y);
+ //}
+//t();
+// console.log(x);
+// console.log(y); 
+// console.log(x);
+// var x=10;
+// console.log(x);
+// console.log(x);
+// let x=10;
+// console.log(x); 
+console.log("ordering production ..........");
+place  
---
+++ JAVASCRIPT/data.js
@@ -0,0 +1,177 @@
+const movies_poster=[
+{ id:1,
+  img :src="https://lumiere-a.akamaihd.net/v1/images/p_disneymovies_avatarthewayofwater_streamingupdate_2096_0908fa1b.jpeg ",
+desc:"The way of water",
+title:"Avatar",
+
+
+},
+
+{ id:2,
+    img :src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5UFXe5DgmRTSTpcLwf_1Oo_C5Pn07JAoHog&usqp=CAU",
+  desc:"second part of OMG movie",
+  title:"OMG2",
+  
+  
+  },
+
+  { id:3,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+
+
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  },
+  { id:1,
+    img :src=" ",
+  desc:"",
+  title:"",
+  
+  
+  }
+]
+movies_poster.map((t)=>{
+
+console.log(t);
+
+
+
+
+})
