5c4a7751fce6b8ee5536f67655c5f44ae0c0c716
tanu <tanu@90141>
2023-08-26 17:34:45 +0530

    fetch data from api

--- react/myapp/src/component/ImageCardSection.jsx
+++ react/myapp/src/component/ImageCardSection.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ImageCard from './ImageCard'
-import CardData from './CardData'
+import { useEffect } from 'react';
+// import CardData from './CardData'
 
 const item=(val)=>{
   return <ImageCard
     key={val.id}
-    src={val.imgsrc}
+    src={val.image}
     title={val.title}
     desc={val.desc}
    />;
    
 }
 
+
 const ImageCardSection = () => {
+  const [Data,setData]=useState([]);
+  useEffect(()=>{
+    const fetchData=async ()=>{
+      try{
+         const Response=await fetch("https://fakestoreapi.com/products")
+         const Data=await Response.json();
+         setData(Data);
+
+      }
+      catch(error){ 
+        console.log("error occured",error);
+      }
+    };
+    fetchData()
+  },[]);
+  
   return (
-    <div className='d-flex justify-content-evenly '>
-       { CardData.map(item) }
+    <div className='d-flex justify-content-evenly flex-wrap' >
+       { Data.map(item) }
     </div>
   )
 }
